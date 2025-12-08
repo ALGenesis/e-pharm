3ad823adfebd5add28a7cf7d8b9fcd1bf74f6b08
ALGenesis <amedeelg02@gmail.com>
2025-12-08 13:49:46 +0100

    update : cart price and quantity

--- script/app/cart.js
+++ script/app/cart.js
@@ -5,8 +5,24 @@ function cart (){
         try {
             const cartInstorage = getCartCard()
 
+            console.log(cartInstorage)
+
+            const articles = cartInstorage?.length ?? 0
+            const totalPrice = cartInstorage?.reduce((acc, cart) => {
+               return acc + parseInt(cart.price)
+            } ,0 )
+
+
             const cartCardCotainer = new CartCardList(cartInstorage)
             cartCardCotainer.appendTo(document.getElementById('cart-container'))
+
+            document.getElementById('total-articles').innerText = articles ?? 0
+            document.getElementById('total-price').innerText = `${totalPrice ?? 0} XAF` 
+
+            document.querySelector('section').addEventListener('productRemoved', () => {
+                document.getElementById('total-articles').innerText = articles ?? 0
+                document.getElementById('total-price').innerText = `${totalPrice ?? 0} XAF` 
+            })
             } 
         catch(e) {
             alert('Impossible de charger les produits')
